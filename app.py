5b7e5a21e0c668178d8380fe5f4f77a0c7d39422
Kunal Pawar <hellokunalpawar@gmail.com>
2022-04-20 19:04:54 +0530

    Added Classifer, Thanks Nishi

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, request, redirect, render_template,url_for
 from resume_rating import file_constants as cnst
 from resume_rating import resume_matcher
 from resume_rating import file_utils
-from resume_classification import resume_classifier
+from Resume_Screening import resume_classifier
 from flask import make_response
 
 
@@ -38,24 +38,24 @@ def failure():
 def success(name):
    return 'Files %s has been selected' %name
 
-@app.route('/classes')
+@app.route('/classify', methods=['POST', 'GET'])
 def classes():
-    if request.method == 'POST':
+    if request.method == 'POST':        
         # check if the post request has the file part
         if 'resume_files' not in request.files:
            flash('Select at least one resume File to proceed further')
            return redirect(request.url)
-        file = request.files['reqFile']
+        print(request.files)
+        
+        # resume_files = request.files['resume_files']
+        # return render_template('resume_classifier.html')
         resume_files = request.files.getlist("resume_files")
         if len(resume_files) == 0:
             flash('Select atleast one resume file to proceed further')
             return redirect(request.url)
-        if ((file and allowed_file(file.filename)) and (len(resume_files) > 0)):
+        if ((len(resume_files) > 0)):
             #filename = secure_filename(file.filename)
             abs_paths = []
-            filename = file.filename
-            req_document = cnst.UPLOAD_FOLDER+'\\'+filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
             for resumefile in resume_files:
                 filename = resumefile.filename
@@ -67,58 +67,46 @@ def classes():
             for file_path in abs_paths:
                 file_utils.delete_file(file_path)
             
-            response = make_response(list(result), 200)
-            response.mimetype = 'text/plain'
-            
             return render_template("resume_classes.html", result=result)
-    
-    return render_template("resume_classes.html", result=str("NIL"))
+    return 'OK'
 
-
-
-@app.route('/ratings', methods=['POST', 'GET'])
-def ratings():
+@app.route('/rating', methods=['POST', 'GET'])
+def check_for_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'reqFile' not in request.files:
-           flash('Job Requirements document can not be empty')
+           flash('Requirements document can not be empty')
            return redirect(request.url)
         if 'resume_files' not in request.files:
            flash('Select at least one resume File to proceed further')
            return redirect(request.url)
+        print(request.files)
         file = request.files['reqFile']
         if file.filename == '':
-           flash('Job Requirements document has not been selected')
+           flash('Requirement document has not been selected')
            return redirect(request.url)
         resume_files = request.files.getlist("resume_files")
         if len(resume_files) == 0:
             flash('Select atleast one resume file to proceed further')
             return redirect(request.url)
         if ((file and allowed_file(file.filename)) and (len(resume_files) > 0)):
-            #filename = secure_filename(file.filename)
-            abs_paths = []
-            filename = file.filename
-            req_document = cnst.UPLOAD_FOLDER+'\\'+filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+           #filename = secure_filename(file.filename)
+           abs_paths = []
+           filename = file.filename
+           req_document = cnst.UPLOAD_FOLDER+'\\'+filename
+           file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+           for resumefile in resume_files:
+               filename = resumefile.filename
+               abs_paths.append(cnst.UPLOAD_FOLDER + '\\' + filename)
+               resumefile.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+           result = resume_matcher.process_files(req_document,abs_paths)
+           for file_path in abs_paths:
+               file_utils.delete_file(file_path)
 
-            for resumefile in resume_files:
-                filename = resumefile.filename
-                
-                abs_paths.append(cnst.UPLOAD_FOLDER + '\\' + filename)
-                resumefile.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-                
-            result = resume_matcher.process_files(req_document, abs_paths)
-            for file_path in abs_paths:
-                file_utils.delete_file(file_path)
-
-            response = make_response(list(result), 200)
-            response.mimetype = 'text/plain'
-            return render_template("resume_results.html", result=result)
+           return render_template("resume_results.html", result=result)
         else:
-            flash('Allowed file types are txt, pdf, png, jpg, jpeg, docx')
-            return redirect(request.url)
-    
-    return render_template("resume_results.html", result=str("NIL"))
+           flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+           return redirect(request.url)
 
 if __name__ == "__main__":
     app.run(debug = True)
--- resume_rating/textract_processing.py
+++ resume_rating/textract_processing.py
@@ -5,4 +5,5 @@ def get_content_as_string(filename):
     text = textract.process(filename)
     lower_case_string =  str(text.decode('utf-8')).lower()
     #final_string = re.sub('[^a-zA-Z0-9 \n]', '', lower_case_string)
+    print(lower_case_string)
     return lower_case_string
